74630e28849075bd8dc22c249aa30d53666d6034
Jingtian Liu <liujt1205@gmail.com>
2022-10-28 20:30:19 -0500

    small changes

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
 
-mongoose.connect('mongodb://localhost/SDC');
+
+mongoose.connect(`mongodb://${process.env.DBIP}:${process.env.DBPORT}/SDC`);
 
 const productSchema = new mongoose.Schema({
   id: Number,
@@ -15,56 +17,4 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('product', productSchema);
 
-// const styleSchema = new mongoose.Schema({
-//   id: Number,
-//   productId: Number,
-//   name: String,
-//   sale_price: String,
-//   original_price: String,
-//   default_style: Boolean,
-//   skus: [Object],
-//   photos: [Object]
-// })
-
-// const Style = mongoose.model('style', styleSchema);
-
-// const cartSchema = new mongoose.Schema({
-//   sku_id: Number,
-//   count: Number
-// })
-
-// const Cart = mongoose.model('cart', cartSchema);
-
-// const skuSchema = new mongoose.Schema({
-//   id: String,
-//   styleId: Number,
-//   size: String,
-//   quantity: Number
-// })
-
-// const Sku = mongoose.model('sku', skuSchema);
-
-// const featureSchema = new mongoose.Schema({
-//   id: Number,
-//   product_id: Number,
-//   feature: String,
-//   value: String
-// });
-
-// const Feature = mongoose.model('feature', featureSchema);
-
-// const photoSchema = new mongoose.Schema({
-//   id: Number,
-//   styleId: Number,
-//   url: String,
-//   thumbnail_url: String
-// });
-
-// const Photo = mongoose.model('photo', photoSchema);
-
 module.exports.Product = Product;
-// module.exports.Style = Style;
-// module.exports.Cart = Cart;
-// module.exports.Sku = Sku;
-// module.exports.Feature = Feature;
-// module.exports.Photo = Photo;
